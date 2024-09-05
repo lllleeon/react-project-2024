716c3b93ce1e29d9885aa1879054eacd0d000f73
lllleeon <goodjowon@gmail.com>
2024-09-05 16:21:55 +0900

    css

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './chp03/book.css'
 import './ch04/css.css'
+import '/ch04/Clock.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import lirary from "./chp03/lirary";
 import ConfirmDialog from "./ch04/ConfirmDialog";
+import Clock from "./ch04/Clock";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval(()=>{root.render(
+    <React.StrictMode>
+        <Clock/>
+    </React.StrictMode>
+);},1000)
+
 root.render(
   <React.StrictMode>
-      <ConfirmDialog />
+      <Clock/>
   </React.StrictMode>
 );
 
